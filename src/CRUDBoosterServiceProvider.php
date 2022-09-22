7df5b3bd754d6558233be576e3525d2dd4dbfc45
Milos Cvijic <mcvijic01@gmail.com>
2022-09-22 19:17:58 +0200

    Update CRUDBoosterServiceProvider.php

--- src/CRUDBoosterServiceProvider.php
+++ src/CRUDBoosterServiceProvider.php
@@ -1,4 +1,4 @@
-<?php namespace crocodicstudio/crudbooster;
+<?php namespace crocodicstudio\crudbooster;
 
 use crocodicstudio\crudbooster\commands\CrudboosterVersionCommand;
 use crocodicstudio\crudbooster\commands\Mailqueues;
